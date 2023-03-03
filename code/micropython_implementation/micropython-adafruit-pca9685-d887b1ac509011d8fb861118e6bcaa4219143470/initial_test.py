07488698746a29d3b86fa6031172dc1dcba59dd8
Andre Maia Chagas <andremaia.chagas@gmail.com>
2023-03-03 15:07:21 +0000

    solved bug with I2C board

--- code/micropython_implementation/micropython-adafruit-pca9685-d887b1ac509011d8fb861118e6bcaa4219143470/initial_test.py
+++ code/micropython_implementation/micropython-adafruit-pca9685-d887b1ac509011d8fb861118e6bcaa4219143470/initial_test.py
@@ -10,7 +10,7 @@ sda=Pin(26)
  
 
 # i2c = I2C(1,scl=Pin(26),sda=Pin(25))
-i2c=machine.SoftI2C(scl=scl,sda=sda,freq=100000)
+i2c=machine.SoftI2C(scl=scl,sda=sda)
 # addresses = i2c.scan()
 # print(addresses)
 address = i2c.scan()
@@ -18,21 +18,26 @@ print(address)
 address = address[0]
 
 time.sleep_ms(10)
-pca= pca9685.PCA9685(i2c=i2c,address=address)
-pca.freq=50
+#pca= pca9685.PCA9685(i2c=i2c,address=address)
+#pca.freq=50
 
-for i in range(16):
+
     #pca.pwm()
-    pca.duty(i,100)
-# ser=servo.Servos(i2c=i2c,address=address)
-# for ind in range(10):
-#     print(ind)
-#     for i in range(0,120,5):
-#         ser.position(0,degrees=i)
-#         time.sleep_ms(5)
-#     for i in range(120,0,-5):
-#         ser.position(0,degrees=i)
-#         time.sleep_ms(5)
-#     time.sleep_ms(200)
+    #pca.duty(i,100)
+ser=servo.Servos(i2c=i2c)
+#ser.position(3,duty=4096)
 
-# ser=servo.Servos(i2c=i2c)
+for ind in range(10):
+     print(ind)
+     for i in range(10,100,10):
+         ser.position(3,degrees=i)
+         #ser.release(3)
+         time.sleep_ms(100)
+         
+     for i in range(90,0,-10):
+         ser.position(3,degrees=i)
+         #ser.release(3)
+         time.sleep_ms(100)
+     #time.sleep_ms(200)
+ser.release(3) 
+ # ser=servo.Servos(i2c=i2c)
